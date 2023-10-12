7f41921dcf3bd6efeaaae025d59e8f050f49e2e8
Rohit kashyap <125393366+RohitXcodes@users.noreply.github.com>
2023-10-12 08:49:21 +0000

    container config done

---
+++ basics/02_data_type.js
@@ -0,0 +1,31 @@
+"use strict"
+ /* treat all js code as newer version of code 
+and once you write use strict , then you can't use older javasript version or defined js .*/
+
+// alert( 3 + 5) // we are using nodejs , not browser
+
+console.log(3 +
+    3); // code readability should be high
+
+console.log("hello , its me ....... i hope your doing good");
+
+let name = "Rohit"
+let age =  20
+let isLoggedIn =  true
+let state = null
+let gender;
+
+// Data types
+// Number => range is 2 to power 53
+// bigint 
+// String => ""
+// boolena => true / false
+// Null => standalone value( it is the representation of empty value)
+// Undefined => 
+//symbol => unique representation
+
+// Objects
+
+console.log(typeof " rohit");
+console.log(typeof null) // output is object
+console.log(typeof undefined) // output is undefined 
