95e6e527fb7c70bb285abe3cedd3874c3e57ab6e
Rohit kashyap <125393366+RohitXcodes@users.noreply.github.com>
2023-10-12 09:18:33 +0000

    conversion of data types

---
+++ basics/03_conversionOperation.js
@@ -0,0 +1,44 @@
+// let score = 33
+
+// let score = " 33 "
+
+// let score = " 33 abc "
+
+// let score = null
+
+// let score = undefined
+
+// let score = true
+
+// let score = " Rohit "
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // when input like 33abc then output is (NaN) means not a number
+
+// "33" => 33
+// "33abc" => NaN
+// ture => 1 ;  false => 0
+
+//let isloggedIn = 1
+//let isloggedIn = 0
+
+//let isloggedIn = "" 
+// let isloggedIn = "Rohit"
+
+// let boolean = Boolean(isloggedIn)
+// console.log(boolean);
+
+// 1 => true ;  0 => false
+//"" => false
+// "rohit" => true
+
+let number = 33
+let stringNumber =  String(number)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
