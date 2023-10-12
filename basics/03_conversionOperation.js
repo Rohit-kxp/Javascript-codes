18ffc4213e0ea4f7cdae6c5c39f7871b954cdab5
Rohit kashyap <125393366+RohitXcodes@users.noreply.github.com>
2023-10-12 09:56:22 +0000

    conversion of data types

--- basics/03_conversionOperation.js
+++ basics/03_conversionOperation.js
@@ -36,9 +36,51 @@
 //"" => false
 // "rohit" => true
 
-let number = 33
-let stringNumber =  String(number)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let number = 33
+// let stringNumber =  String(number)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 
+//*************************************** Operations ************************************************//
+
+let value =  3
+let negvalue = -value
+//console.log(negvalue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2/2) // this is for divide two or more numbers
+// console.log(2*2)
+// console.log(2**3)// for 2 to power 3
+// console.log(2%2) // this is for remainder
+
+let str1 = "Hello"
+let str2 = " Rohit"
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log( 1 + 2 )
+// console.log( "1" + 2 ) // if you 1st enter the string / Number... then whole operations is consider for string 
+// console.log( 1 + "2" )
+// console.log( "1" + "2" )
+// console.log( "1" + 2 + 2 )
+// console.log( 1 + 2 + "2" ) // but if stirng in last and numbers in first " toh phle number add honge uske baad jo number ayega voh sting ke saath jayega"
+
+//best way to write these types of codes ......like 
+
+// console.log( (3 + 4) * 5 % 6);
+
+// console.log(true);
+// console.log(+true); // + is used for tricky conversions...
+// console.log( + ""); // output is zero becuase now this is a converison
+// A gentle remainder please do not use this conversion method , this is not a good way to write a code
+
+
+let num1 , num2 , num3 
+num1 = num2 = num3 = 2+2
+
+let gamecounter = 100
+// gamecounter++ ;
+++gamecounter;
+console.log(gamecounter);
